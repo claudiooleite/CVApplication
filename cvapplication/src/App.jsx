cf42085d7137397df36797ae597fbe5a9ea9a3fb
claudiooleite <152962012+claudiooleite@users.noreply.github.com>
2024-03-30 15:38:07 +0100

    app structured

--- cvapplication/src/App.jsx
+++ cvapplication/src/App.jsx
@@ -1,6 +1,10 @@
 
 import './App.css'
 import  FormGroupExample from "./form/form";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
     
@@ -9,10 +13,23 @@ function App() {
 
   return (
     <>
-    <FormGroupExample>
-      
-    </FormGroupExample>
-
+      <header>
+        <div>
+          <h1>CV App</h1>
+        </div>
+      </header>
+      <main>
+        <Container fluid="md">
+          <Row>
+            <Col>
+              <FormGroupExample/>
+            </Col>
+            <Col>
+              <form action="">Result</form>
+            </Col>
+          </Row>
+        </Container>
+      </main>
     </>
   )
 }
