0832ff20fc0013ebc8aa6492595f37a7e1e2ff70
claudiooleite <152962012+claudiooleite@users.noreply.github.com>
2024-04-25 12:35:07 +0200

    btn code updated with info from https://www.convertapi.com/ docs

--- src/components/PdfGenerator.jsx
+++ src/components/PdfGenerator.jsx
@@ -1,40 +1,30 @@
-import axios from 'axios';
+import React from 'react';
+import ConvertApi from 'convertapi-js';
 
-const ConvertToPdf = async (htmlContent) => {
+const ConvertToPdf = async () => {
   try {
-    const apiKey = 'evIVnlwRTp9RdC9C'; 
-    const apiUrl = 'https://v2.convertapi.com/convert/web/to/pdf';
-    
-    const response = await axios.post(apiUrl, {
-      Parameters: [
-        {
-          Name: 'Html',
-          Value: htmlContent
-        }
-      ],
-      ConversionTimeout: 180 // 3 minutes timeout
-    }, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`
-      },
-      responseType: 'arraybuffer' // response type as arraybuffer to get PDF file
-    });
+    // Initialize ConvertAPI client with your API key
+    let convertApi = ConvertApi.auth('evIVnlwRTp9RdC9C');
+    // Create parameters for conversion
+    let params = convertApi.createParams();
 
-    // Create a blob from the response data
-    const blob = new Blob([response.data], { type: 'application/pdf' });
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.style.display = 'none';
-    a.href = url;
-    a.download = 'converted.pdf';
-    document.body.appendChild(a);
-    a.click();
-    window.URL.revokeObjectURL(url);
-    document.body.removeChild(a);
-    
+    // Add HTML file to convert (replace with your actual HTML content or URL)
+    params.add('File', new URL('https://odincvapp.netlify.app/'));
+
+    // Perform conversion from HTML to PDF
+    let result = await convertApi.convert('htm', 'pdf', params);
+
+    // Handle the result (e.g., download the converted PDF)
+    if (result && result.file) {
+      // Create a download link for the converted PDF
+      const downloadLink = document.createElement('a');
+      downloadLink.href = result.file.url;
+      downloadLink.download = 'converted.pdf';
+      downloadLink.click();
+    }
   } catch (error) {
     console.error('Error converting to PDF:', error);
+    // Handle or display the error message to the user
   }
 };
 
