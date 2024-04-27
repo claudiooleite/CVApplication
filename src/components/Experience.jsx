70b828c1aa9630dcd15f34064179e9676c18d4cd
claudiooleite <152962012+claudiooleite@users.noreply.github.com>
2024-04-27 14:12:09 +0200

    experience component refactured

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,61 +1,83 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-  
-function Experience({onAddExperience}){
-      
-    
-    const [title, setTitle] = useState('');
-    const [companyName, setCompanyName] = useState('');
-    const [location, setLocation] = useState('');
-    const [startDate, setStartDate] = useState('');
-    const [endDate, setEndDate] = useState('');
+function Experience({ onAddExperience }) {
+  const [experienceData, setExperienceData] = useState({
+    title: '',
+    companyName: '',
+    location: '',
+    startDate: '',
+    endDate: ''
+  });
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setExperienceData({ ...experienceData, [name]: value });
+  };
 
   const handleAddExperienceClick = (e) => {
     e.preventDefault();
-    const newExperienceEntry = { title, companyName, location, startDate, endDate};
-    onAddExperience(newExperienceEntry);
-    setTitle('');
-    setCompanyName('');
-    setLocation('');
-    setStartDate('');
-    setEndDate('');
+    onAddExperience(experienceData);
+    setExperienceData({
+      title: '',
+      companyName: '',
+      location: '',
+      startDate: '',
+      endDate: ''
+    });
   };
-  
-    
-    return (
+
+  return (
     <Form>
       <Form.Group className="mb-3" controlId="controlTitle">
         <Form.Label>Title</Form.Label>
-        <Form.Control value={title} onChange={(e) => setTitle(e.target.value)} type="text" />
-        
+        <Form.Control
+          name="title"
+          value={experienceData.title}
+          onChange={handleChange}
+          type="text"
+        />
       </Form.Group>
       <Form.Group className="mb-3" controlId="controlName">
         <Form.Label>Company Name</Form.Label>
-        <Form.Control value={companyName} onChange={(e) => setCompanyName(e.target.value)} type="text" />
-        
+        <Form.Control
+          name="companyName"
+          value={experienceData.companyName}
+          onChange={handleChange}
+          type="text"
+        />
       </Form.Group>
       <Form.Group className="mb-3" controlId="controlLocation">
         <Form.Label>Location</Form.Label>
-        <Form.Control type="text"  value={location} onChange={(e) => setLocation(e.target.value)} />
-        
+        <Form.Control
+          name="location"
+          value={experienceData.location}
+          onChange={handleChange}
+          type="text"
+        />
       </Form.Group>
       <Form.Group className="mb-3" controlId="controlStart">
         <Form.Label>Start Date</Form.Label>
-        <Form.Control  value={startDate} onChange={(e) => setStartDate(e.target.value)} type="date" />
-        
+        <Form.Control
+          name="startDate"
+          value={experienceData.startDate}
+          onChange={handleChange}
+          type="date"
+        />
       </Form.Group>
       <Form.Group className="mb-3" controlId="controlEnd">
         <Form.Label>End Date</Form.Label>
-        <Form.Control value={endDate} onChange={(e) => setEndDate(e.target.value)} type="date" />
-        
+        <Form.Control
+          name="endDate"
+          value={experienceData.endDate}
+          onChange={handleChange}
+          type="date"
+        />
       </Form.Group>
       <Button onClick={handleAddExperienceClick}>Add Experience</Button>
     </Form>
   );
-
 }
 
 export default Experience;
